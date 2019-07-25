5b45660f5d3ad7ce89c60da89cf0c115fc0cb58b
Amehmeto <42amehmeto@gmail.com>
2019-07-25 22:14:56 +0200

    Adding 6 failing tests

--- day02/ex02/test_loupe.php
+++ day02/ex02/test_loupe.php
@@ -37,6 +37,20 @@ class LoupeTests extends TestCase {
 		);
 	}
 
+	public function testHTMLWithTitleAttributeAndLinkSpreadOut(){
+		$this->assertShellExec(
+			'title_spread_out',
+			"<html><head><title>Nice page</title></head>\n<body>Hello World <a href=\"http://cyan.com\" title=\"UN LIEN\">CECI\nEST\nUN\nLIEN</a>\n"
+		);
+	}
+
+	public function testHTMLWithTitleAttributeAndLinkSpreadOut2(){
+		$this->assertShellExec(
+			'title_spread_out2',
+			"<html><head><title>Nice page</title></head>\n<body>Hello World <a\nhref=\"http://cyan.com\"\ntitle=\"UN LIEN\">CECI\nEST\nUN\nLIEN</a>\n"
+		);
+	}
+
 	public function testSeveralLinesHTMLWithTitleAttributeLinkAndImage(){
 		$this->assertShellExec(
 			'title_and_image',
@@ -55,16 +69,39 @@ class LoupeTests extends TestCase {
 		$this->assertShellExec(
 			'wrong_motherfucker', 
 			"<html><head><title>Nice page</title></head>\n<body>Hello World <a href=\"http://cyan.com\" title=\"UN LIEN\">CECI EST UN LIEN</a>\n<br /><a href=http://www.riven.com> ET CA AUSSI <img src=wrong.image title=\"ET ENCORE CA\" alt=\"un lien\"></a>\n<div title=\"ceci est une erreur\"> et ca aussi <img src=wrong.image title=\"et encore ca\" alt=\"un lien\"><div>\n</body></html>\n"
-
 		);
 	}
 
-	/*
-	public function testOneLineSeveralTitleAttributesAndAHREF(){
+	public function testShouldOnlyMatchTitleAttributesInsideAHrefTags3()
+    {
+        $this->assertShellExec(
+            'wrong_motherfucker2',
+            "<html><head><title>Nice page</title></head>\n<body>Hello World <a href=\"http://cyan.com\" title=\"UN LIEN\">CECI EST UN LIEN</a>\n<br /><a href=http://www.riven.com><img src=wrong.image title=\"ET ENCORE CA\" alt=\"un lien\"> ET CA AUSSI </a>\n<div title=\"ceci est une erreur\"> et ca aussi <img src=wrong.image title=\"et encore ca\" alt=\"un lien\"><div>\n</body></html>\n"
+        );
+    }
+
+	public function testTextImageTagTextSandwich()
+    {
+        $this->assertShellExec(
+            'text_image_tag_text_sandwich',
+            "<html><head><title>Nice page</title></head>\n<body>Hello World <a href=\"http://cyan.com\" title=\"UN LIEN\">CECI EST UN LIEN</a>\n<br /><a href=http://www.riven.com> ET CA AUSSI <img src=wrong.image title=\"ET ENCORE CA\" alt=\"un lien\"> ET CA AUSSI </a>\n<div title=\"ceci est une erreur\"> et ca aussi <img src=wrong.image title=\"et encore ca\" alt=\"un lien\"><div>\n</body></html>\n"
+        );
+    }
+
+	public function testImageTagTextImageTagSandwich()
+	{
 		$this->assertShellExec(
-			'title_and_image_double',
-			"<html><head><title>Nice page</title></head> <body>Hello World <a href=\"http://cyan.com\" title=\"UN LIEN\">CECI EST UN LIEN</a> <br /><a href=http://www.riven.com> ET CA AUSSI <img src=wrong.image title=\"ET ENCORE CA\"></a> </body></html> <html><head><title>Nice page</title></head> <body>Hello World <a href=\"http://cyan.com\" title=\"UN LIEN\">CECI EST UN LIEN</a> <br /><a href=http://www.riven.com> ET CA AUSSI <img src=wrong.image title=\"ET ENCORE CA\"></a> </body></html>"
+			'image_tag_text_image_tag_sandwich',
+			"<html><head><title>Nice page</title></head>\n<body>Hello World <a href=\"http://cyan.com\" title=\"UN LIEN\">CECI EST UN LIEN</a>\n<br /><a href=http://www.riven.com><img src=wrong.image title=\"ET ENCORE CA\" alt=\"un lien\"> ET CA AUSSI <img src=wrong.image title=\"ET ENCORE CA\" alt=\"un lien\"></a>\n<div title=\"ceci est une erreur\"> et ca aussi <img src=wrong.image title=\"et encore ca\" alt=\"un lien\"></div>\n</body></html>\n"
 		);
-	}*/
+	}
+
+	public function testImageTagTextImageTagSandwichSeveralLineSpreadOut()
+	{
+		$this->assertShellExec(
+			'image_tag_text_image_tag_sandwich_spread_out',
+			"<html><head><title>Nice page</title></head>\n<body>Hello World <a href=\"http://cyan.com\" title=\"UN LIEN\">CECI EST UN LIEN</a>\n<br /><a href=http://www.riven.com><img src=wrong.image title=\"ET ENCORE CA\" alt=\"un lien\">\n ET CA AUSSI \n<img src=wrong.image \ntitle=\"ET ENCORE CA\" alt=\"un lien\"></a>\n<div title=\"ceci est une erreur\"> et ca\n aussi <img src=wrong.image title=\"et encore ca\" alt=\"un lien\"></div>\n</body></html>\n"
+		);
+	}
 }
 ?>
