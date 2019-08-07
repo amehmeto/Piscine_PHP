44cf6679e851c5f76b624593c78640365c2cf8d5
Amehmeto <42amehmeto@gmail.com>
2019-08-07 15:06:55 +0200

    ex06 passes login and password tests

--- day03/ex06/members_only.php
+++ day03/ex06/members_only.php
@@ -1,13 +1,37 @@
 <?php
-if (!isset($_SERVER['PHP_AUTH_USER'])) {
+
+function surroundContent($content) {
+    $opening_tags = "<html><body>";
+    $closing_tags = "</body></html>\n";
+
+    return $opening_tags . $content . $closing_tags;
+}
+
+function encodeImage(){
+    $file_path = "../img/42.png";
+    $MIME_type = mime_content_type($file_path);
+    $file_content = file_get_contents($file_path);
+
+    return "data:" . $MIME_type . ";base64," . base64_encode($file_content);
+}
+
+function generateContent(){
+    $content = "\nBonjour Zaz<br />\n";
+    $content .= "<img src='" . encodeImage() . "'>\n";
+
+    return $content;
+}
+
+if (isset($_SERVER['PHP_AUTH_USER'])
+            AND $_SERVER['PHP_AUTH_USER'] === 'zaz' AND $_SERVER['PHP_AUTH_PW'] === 'zaz') {
+    $content = generateContent();
+    $content = surroundContent($content);
+    echo $content;
+}
+else {
     header('WWW-Authenticate: Basic realm="Tag img');
     header('HTTP/1.0 401 Unauthorized');
-    echo "Bouge de la batard\n";
+    $forbidden_area_message = "Cette zone est accessible uniquement aux membres du site";
+    echo surroundContent($forbidden_area_message);
+    exit;
 }
-else
-{
-    echo "Bonjour " . $_SERVER['PHP_AUTH_USER'] . "\n";
-}
-
-
-?>
--- day03/ex06/test_members_only.php
+++ day03/ex06/test_members_only.php
@@ -4,16 +4,32 @@ use PHPUnit\Framework\TestCase;
 
 class MembersOnlyTests extends TestCase {
 
-    const PROGRAM = 'curl --user';
-    const LOGIN_PASSWORD = 'zaz:jaimelespetitsponeys';
     const PATH = 'http://localhost:80/PhpstormProjects/piscine_php/day03/ex06/members_only.php';
-    private $_cmd_line = self::PROGRAM . ' ' . self::LOGIN_PASSWORD . ' ' . self::PATH;
+
+    private function generateCmd($program, $login, $password){
+        return $program . ' ' . $login . ':' . $password . ' ' . self::PATH;
+    }
 
     public function testBonjour(){
-        $output = shell_exec($this->_cmd_line);
-        $expected_result = "Bonjour zaz\n";
+        $cmd_line = $this->generateCmd('curl --user', 'zaz', 'zaz');
+        $output = shell_exec($cmd_line);
+        $file_content = file_get_contents("../img/42.png");
+        $expected_result = "<html><body>\nBonjour Zaz<br />\n<img src='data:image/png;base64," .
+                            base64_encode($file_content) . "'>\n</body></html>\n";
+        $this->assertSame($expected_result, $output);
+    }
+
+    public function testWrongPassword(){
+        $cmd_line = $this->generateCmd('curl -v --user', 'zaz', 'wesh alors');
+        $output = shell_exec($cmd_line);
+        $expected_result = "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>\n";
+        $this->assertSame($expected_result, $output);
+    }
+
+    public function testWrongUsername(){
+        $cmd_line = $this->generateCmd('curl -v --user', 'zsadfaz', 'zaz');
+        $output = shell_exec($cmd_line);
+        $expected_result = "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>\n";
         $this->assertSame($expected_result, $output);
     }
 }
-
-?>
