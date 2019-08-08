3ae89276d25c432b36792f41169be51b2535ef6a
Amehmeto <42amehmeto@gmail.com>
2019-08-08 19:23:19 +0200

    refactoring

--- day04/ex02/create.php
+++ day04/ex02/create.php
@@ -1,19 +1,33 @@
 <?php
 
-$error_message = "ERROR\n";
+function createPrivateDirectory(){
+    $path = "../private";
 
-function storeCredentials($credentials){
-
-    if (!file_exists("../private"))
-        if (!mkdir("../private", 0700))
+    if (!file_exists($path))
+        if (!mkdir($path, 0700))
             echo "Directory creation failed\n";
+}
+
+function prepareNewCredentials($credentials){
     unset($credentials['submit']);
     $credentials['passwd'] = hash('whirlpool', $credentials['passwd']);
     $credentials_list = array($credentials);
+    return $credentials_list;
+}
+
+function buildCredentialsList($credentials){
+    $credentials_list = prepareNewCredentials($credentials);
     $serialized_list = serialize($credentials_list);
-    file_put_contents("../private/passwd", $serialized_list, FILE_APPEND);
+    return $serialized_list;
+}
+function storeCredentials($new_credentials){
+    createPrivateDirectory();
+    $credentials_list = buildCredentialsList($new_credentials);
+    file_put_contents("../private/passwd", $credentials_list, FILE_APPEND);
     echo "OK\n";
 }
+
+$error_message = "ERROR\n";
 
 if (!isset($_POST['login']) OR !isset($_POST['passwd']))
     echo $error_message;
