0fbc72cb9c77a9568e421cc664f808537f57eeca
Amehmeto <42amehmeto@gmail.com>
2019-08-08 21:50:38 +0200

    able to add several credentials to the list

--- day04/ex02/create.php
+++ day04/ex02/create.php
@@ -11,19 +11,27 @@ function createPrivateDirectory(){
 function prepareNewCredentials($credentials){
     unset($credentials['submit']);
     $credentials['passwd'] = hash('whirlpool', $credentials['passwd']);
-    $credentials_list = array($credentials);
-    return $credentials_list;
+    return $credentials;
 }
 
 function buildCredentialsList($credentials){
-    $credentials_list = prepareNewCredentials($credentials);
+    $credentials = prepareNewCredentials($credentials);
+    $path = "../private/passwd";
+    if (!file_exists($path)) {
+        $credentials_list = array();
+    }
+    else {
+        $serialized_list = file_get_contents($path);
+        $credentials_list = unserialize($serialized_list);
+    }
+    array_push($credentials_list, $credentials);
     $serialized_list = serialize($credentials_list);
     return $serialized_list;
 }
 function storeCredentials($new_credentials){
     createPrivateDirectory();
     $credentials_list = buildCredentialsList($new_credentials);
-    file_put_contents("../private/passwd", $credentials_list, FILE_APPEND);
+    file_put_contents("../private/passwd", $credentials_list);
     echo "OK\n";
 }
 
--- day04/ex02/test_create.php
+++ day04/ex02/test_create.php
@@ -5,7 +5,7 @@ use \PHPUnit\Framework\TestCase;
 class CreateAccountTests extends TestCase {
 
     const PATH = "http://localhost:80/PhpstormProjects/piscine_php/day04/ex02/create.php";
-    const PROGRAM = "curl ";
+    const PROGRAM = "curl -s ";
 
     private function generateCmdLine($login, $password){
         $login = "-d login=" . $login . " ";
@@ -48,8 +48,10 @@ class CreateAccountTests extends TestCase {
     }
 
     public function testSeveralValidCredentialsCredentials(){
+        $cmd_line = $this->generateCmdLine("JUL", "weshalors");
+        shell_exec($cmd_line);
         $output = shell_exec("cat ../private/passwd");
-        $expected_output = "";
+        $expected_output = "a:2:{i:0;a:2:{s:5:\"login\";s:5:\"toto1\";s:6:\"passwd\";s:128:\"2bdd45b3c828273786937ac1b4ca7908a431019e8b93c9fd337317f92fac80dace29802bedc33d9259c8b55d1572cb8a6c1df8579cdaa02256099ed52a905d38\";}i:1;a:2:{s:5:\"login\";s:3:\"JUL\";s:6:\"passwd\";s:128:\"f4e5c9514ed0ca9d37b79ffc67e71853f83210c431ce13dd24ee1effd206d338bed72fc9424deaff5d6bbb31db668449eaeb95400a1a1438ea9a2942873c0f6b\";}}";
         $this->assertSame($expected_output, $output);
     }
 }
